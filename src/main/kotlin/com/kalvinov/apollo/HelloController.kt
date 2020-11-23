f9c1fc88b26f423f6aa100cce0b23c09bbf38470
Anton <anton.kalvinov@semrush.com>
2020-11-23 17:01:32 +0300

    Initial commit

---
+++ src/main/kotlin/com/kalvinov/apollo/HelloController.kt
@@ -0,0 +1,18 @@
+package com.kalvinov.apollo
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = "Blog"
+        logger.info("Sample of log message")
+        return "blog"
+    }
+}
---
+++ src/main/kotlin/com/kalvinov/apollo/KotlinDemoApplication.kt
@@ -0,0 +1,10 @@
+package com.kalvinov.apollo
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDemoApplication>(*args)
+}
